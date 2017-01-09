089c6dd844f3d9b4536b6070ccbcad2bbf6ccdd5
Mateusz Dominiak <matteuszd@gmail.com>
2017-01-10 00:42:06 +0100

    fun with camera

--- Day19 - Webcam Fun/scripts.js
+++ Day19 - Webcam Fun/scripts.js
@@ -23,14 +23,51 @@ function paintToCanvas() {
   canvas.width = width;
   canvas.height = height;
   return setInterval(() => {
-    ctx.drawImage(video, 0, 0, width, height)
+    ctx.drawImage(video, 0, 0, width, height);
+    //take the pixels out
+    let pixels = ctx.getImageData(0,0,width,height);
+    //mess with them
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    //put them back
+    ctx.putImageData(pixels, 0, 0)
+
   }, 16);
 }
 
 function takePhoto() {
+  // played the sound
   snap.currentTime = 0;
   snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firsChild);
 }
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 200] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 250] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+
 
 getVideo();
 video.addEventListener('canplay', paintToCanvas);
